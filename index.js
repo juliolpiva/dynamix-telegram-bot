d2b041a2ba25789f8f450b5e8928f06b0c950476
JulioLP <julio.piva@present-technologies.com>
2020-04-27 23:19:36 +0100

    Add some logs

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ const bot = new TeleBot(process.env.BOT_TOKEN);
 // On every type of message (& command)
 bot.on(['*', '/*'], (msg, self) => {
 
-    let id = msg.from.id;
-    let replyToMessage = msg.message_id;
-    let parseMode = 'html';
+    const id = msg.from.id;
+    const fromName = msg.from.first_name;
+    const replyToMessage = msg.message_id;
+    const parseMode = 'html';
 
     const isDaniel = msg.text.toLowerCase() === 'daniel';
 
-    return bot.sendMessage(
-        id, `Vamos mudar de assunto galera! brinks`, {replyToMessage, parseMode}
-    );
+    console.log("Group Message:", msg.text, " | From:", fromName);
+
+    if(isDaniel) {
+        return bot.sendMessage(
+            id, `Vamos mudar de assunto galera! brinks`, {replyToMessage, parseMode}
+        );
+    }
 });
 
 bot.start();
