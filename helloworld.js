bfbbe04d83283c6e08b606c0b4c1f5df2b9fd179
JulioLP <julio.piva@present-technologies.com>
2020-04-27 22:40:56 +0100

    Add the token from ENV

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,15 @@
 import TeleBot from 'telebot';
 
-const bot = new TeleBot('TELEGRAM_BOT_TOKEN');
+const bot = new TeleBot(process.env.BOT_TOKEN);
 
 // On every type of message (& command)
 bot.on(['*', '/*'], (msg, self) => {
+    
     let id = msg.from.id;
     let replyToMessage = msg.message_id;
     let type = self.type;
     let parseMode = 'html';
+
     return bot.sendMessage(
         id, `This is a <b>${ type }</b> message.`, {replyToMessage, parseMode}
     );
