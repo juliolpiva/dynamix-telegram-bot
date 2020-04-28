ebaf82365fa5fc7c10f40bba394181ecf210e17d
JulioLP <julio.piva@present-technologies.com>
2020-04-28 09:12:54 +0100

    Apply some changes for netlify

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,41 +1,22 @@
 const Telegraf = require('telegraf');
 const dotenv = require('dotenv');
 
+const startAction = require('./actions/start');
+
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.use(async (ctx, next) => {
-    const start = new Date()
+bot.start(ctx => {
+    return startAction(ctx)
+})
 
-    const message = ctx.update.message.text;
-    const user = ctx.update.message.chat.first_name;
-
-    console.log(ctx);
-
-    console.log('Message:', message, ' | From:', user);
-
-    if (message && message.toLowerCase().includes('help')) {
-        ctx.reply('Cara, não seja noob, usa /amarelo ou /vermelho, não tem mistério! e deixa de ser vadio e ajuda no projeto!')
+exports.handler = async event => {
+    try {
+        await bot.handleUpdate(JSON.parse(event.body));
+        return { statusCode: 200, body: '' };
+    } catch (e) {
+        console.log(e)
+        return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
     }
-
-    if (message && message.toLowerCase().includes('amarelo')) {
-        ctx.reply('IIIIH TOMOU AMARELO!')
-    }
-
-    if (message && message.toLowerCase().includes('vermelho')) {
-        ctx.reply('PESADO, TA EXPULSO!!')
-    }
-
-    if (message && message.toLowerCase().includes('matias')) {
-        ctx.reply('ARGENTINO');
-    }
-
-    await next()
-
-    const ms = new Date() - start
-
-    console.log('Response time: %sms', ms)
-  })
-
-  bot.launch()
+}
