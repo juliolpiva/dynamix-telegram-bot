5d7f4cad1a1c0eb4405ef6d54ebdfacb1ade91bd
JulioLP <julio.piva@present-technologies.com>
2020-04-28 10:54:32 +0100

    Add new commands
    - start
    - amarelo
    - vermelho
    - help

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,19 +1,42 @@
 const Telegraf = require('telegraf');
 const dotenv = require('dotenv');
 
-const startAction = require('./actions/start');
-
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx => {
-    return startAction(ctx)
-})
+    ctx.reply('Bem vindo caro amigo, use /help para mais informações!')
+});
+
+bot.command('amarelo', ctx => {
+    console.log('amarelo', ctx);
+
+    ctx.reply('IIIIH TOMOU AMARELO!')
+});
+
+bot.command('vermelho', ctx => {
+    console.log('vermelho', ctx);
+
+    ctx.reply('PESADO, TA EXPULSO!!')
+});
+
+bot.command('help', ctx => {
+    ctx.reply('Cara, não seja noob, usa /amarelo ou /vermelho, não tem mistério! e deixa de ser vadio e ajuda no projeto!')
+});
+
+bot.on(ctx => {
+    console.log('on', ctx);
+});
+
+bot.launch();
 
 exports.handler = async event => {
     try {
         await bot.handleUpdate(JSON.parse(event.body));
+
+        console.log('New command! :D');
+
         return { statusCode: 200, body: '' };
     } catch (e) {
         console.log(e)
